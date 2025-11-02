5a9864d38a7fabfb440fa572d985eac53a279f24
YMS2005 <yassengamedev@gmail.com>
2025-11-02 23:57:45 +0200

    .

---
+++ src/main/java/javorks/pos/Dashboard.java
@@ -0,0 +1,39 @@
+package javorks.pos;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+
+public class Dashboard extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+
+        // setting the nodes for the scene
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+            // setting the scene and stage
+            //Group root = new Group();
+            Scene sc = new Scene(root);
+            primaryStage.setScene(sc);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+}
---
+++ src/main/java/javorks/pos/HelloController.java
@@ -0,0 +1,14 @@
+package javorks.pos;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
